6011644a9117c3d944d0ecfa51e98cdf7f6b173f
stoicastefan <stoica.stefan14@yahoo.ro>
2023-03-22 19:32:40 +0200

    Fix pop up from hub page

--- static/engine.js
+++ static/engine.js
@@ -16,6 +16,7 @@ let game4 = document.getElementById('game4');
 const game4_coords = game4.getBoundingClientRect();
 console.log(game4_coords.top, game4_coords.right);
 
+popUpCreated = false;
 
 let moveBy = 5;
 window.addEventListener('load', () => {
@@ -33,11 +34,16 @@ window.addEventListener('keydown', (e) => {
 
     if ((Math.abs(companion_coords.top - (game1_coords.top + 160 )) <= 100) && (Math.abs(companion_coords.right - game1_coords.right) <= 100)  ){
         console.log("Touching 1")
+        if (popUpCreated) {
+            const popUp = document.querySelector('.pop-up');
+            popUp.remove();
+            popUpCreated = false;
+          }
         document.getElementById("game1").style.backgroundColor = 'red';
         document.getElementById("game2").style.backgroundColor = '';
         document.getElementById("game3").style.backgroundColor = '';
         document.getElementById("game4").style.backgroundColor = '';
-
+        popUpCreated = true;
           const popUp = document.createElement("div");
           popUp.className = "pop-up";
           const message = document.createElement("p");
@@ -50,16 +56,21 @@ window.addEventListener('keydown', (e) => {
           popUp.appendChild(message);
           popUp.appendChild(button);
           document.body.appendChild(popUp);
-          popUpCreated = true;
+
 
     }
     else if ((Math.abs(companion_coords.top - (game2_coords.top +160 )) <= 100) && (Math.abs(companion_coords.right - game2_coords.right) <= 100)  ){
         console.log("Touching 2")
+        if (popUpCreated) {
+            const popUp = document.querySelector('.pop-up');
+            popUp.remove();
+            popUpCreated = false;
+          }
         document.getElementById("game2").style.backgroundColor = 'red';
         document.getElementById("game1").style.backgroundColor = '';
         document.getElementById("game3").style.backgroundColor = '';
         document.getElementById("game4").style.backgroundColor = '';
-
+        popUpCreated = true;
         const popUp = document.createElement("div");
           popUp.className = "pop-up";
           const message = document.createElement("p");
@@ -75,11 +86,16 @@ window.addEventListener('keydown', (e) => {
     }
     else if ((Math.abs(companion_coords.top - (game3_coords.top + 160 )) <= 100) && (Math.abs(companion_coords.right - game3_coords.right) <= 100)  ){
         console.log("Touching 3")
+        if (popUpCreated) {
+            const popUp = document.querySelector('.pop-up');
+            popUp.remove();
+            popUpCreated = false;
+          }
         document.getElementById("game3").style.backgroundColor = 'red';
         document.getElementById("game2").style.backgroundColor = '';
         document.getElementById("game1").style.backgroundColor = '';
         document.getElementById("game4").style.backgroundColor = '';
-
+        popUpCreated = true;
         const popUp = document.createElement("div");
           popUp.className = "pop-up";
           const message = document.createElement("p");
@@ -95,11 +111,16 @@ window.addEventListener('keydown', (e) => {
     }
     else if ((Math.abs(companion_coords.top - (game4_coords.top + 160 )) <= 100) && (Math.abs(companion_coords.right - game4_coords.right) <= 100)  ){
         console.log("Touching 4")
+        if (popUpCreated) {
+            const popUp = document.querySelector('.pop-up');
+            popUp.remove();
+            popUpCreated = false;
+          }
         document.getElementById("game4").style.backgroundColor = 'red';
         document.getElementById("game2").style.backgroundColor = '';
         document.getElementById("game3").style.backgroundColor = '';
         document.getElementById("game1").style.backgroundColor = '';
-
+        popUpCreated = true;
         const popUp = document.createElement("div");
           popUp.className = "pop-up";
           const message = document.createElement("p");
